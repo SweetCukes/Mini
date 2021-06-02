843d66eeb6b7dc0041245f9c11db97f6c8aaa164
lu_yi <lu_yi@hisuntech.com>
2021-06-02 17:01:57 +0800

    默认变更列表

---
+++ src/main/java/com/mooc/miniprogram/MiniProgramApplication.java
@@ -0,0 +1,13 @@
+package com.mooc.miniprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mooc/miniprogram/MiniProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc.miniprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
